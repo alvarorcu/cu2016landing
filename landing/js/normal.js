c2279a18e23bd48947d2e31e9a656608969bf3c9
Alvaro Concha <aconchauriol@gmail.com>
2015-12-20 03:48:44 -0500

    el index esta casi listo, falta añadir el video en la parte de twitter

--- landing/js/normal.js
+++ landing/js/normal.js
@@ -60,15 +60,6 @@ jQuery(function($) {
 		$('header').height($(window).height());
 	});
 
-	// Pricing Box Click Event
-	$('.pricing .box-main').click(function() {
-		$('.pricing .box-main').removeClass('active');
-		$('.pricing .box-second').removeClass('active');
-		$(this).addClass('active');
-		$(this).next($('.box-second')).addClass('active');
-		$('#pricing').css("background-image", "url(" + $(this).data('img') + ")");
-		$('#pricing').css("background-size", "cover");
-	});
 
 	// Mobile Nav
 	$('body').on('click', 'nav .navbar-toggle', function() {
