883a28779645796dd8204ecead6038e252aecd3f
ZodiacFireworks <martin.vuelta@gmail.com>
2016-01-17 21:24:56 -0500

    deleted testing prefix

--- landing/js/registration.js
+++ landing/js/registration.js
@@ -4,7 +4,7 @@ var R2D2 = null;
     "use strict";
 
     var firebase = new Firebase("https://cu2016.firebaseio.com");
-    var testingPrefix = "/cu2016";
+    var testingPrefix = "";
     var normalizedPath = window.location.pathname.replace(".html","").replace(testingPrefix, "");
     var searchPath = window.location.search.replace("?next=","").replace(testingPrefix, "");
 
